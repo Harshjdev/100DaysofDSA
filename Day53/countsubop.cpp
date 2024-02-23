3f70e4eb5c1837c33de8e4b9aac378654a6bd721
harshjdev <harsh97736@gmail.com>
2024-02-23 22:52:54 +0530

    DAY53

--- Day53/countsubop.cpp
+++ Day53/countsubop.cpp
@@ -1,10 +1,9 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+int findAllSubarraysWithGivenSum(vector<int>& arr, int k) {
     int n = arr.size(); // size of the given array.
-    map mpp;
+    unordered_map<int, int> mpp; // Corrected to use unordered_map with int keys
     int preSum = 0, cnt = 0;
 
     mpp[0] = 1; // Setting 0 in the map.
@@ -25,12 +24,10 @@ int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
     return cnt;
 }
 
-int main()
-{
-    vector arr = {3, 1, 2, 4};
+int main() {
+    vector<int> arr = {3, 1, 2, 4};
     int k = 6;
     int cnt = findAllSubarraysWithGivenSum(arr, k);
     cout << "The number of subarrays is: " << cnt << "\n";
     return 0;
 }
-
